8ecf1142c18b1ca158b8b27cb027a9d52da208c5
beraSayantan <f20211828@goa.bits-pilani.ac.in>
2022-09-02 20:26:15 +0530

    Work on Product Page

--- lib/Tabs/home_tab.dart
+++ lib/Tabs/home_tab.dart
@@ -3,6 +3,7 @@ import 'dart:developer';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:shopping/constants.dart';
+import 'package:shopping/screens/product_page.dart';
 import 'package:shopping/widgets/custom_action_bar.dart';
 
 class HomeTab extends StatelessWidget {
@@ -33,55 +34,63 @@ class HomeTab extends StatelessWidget {
                     bottom: 12.0
                   ),
                   children: snapshot.data!.docs.map((document) {
-                    return Container(
-                      height: 350.0,
-                      margin: EdgeInsets.symmetric(
-                        vertical: 12.0,
-                        horizontal: 24.0,
-                      ),
-                      child: Stack(
-                        children: [
-                          Container(
-                            height: 350.0,
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(12.0),
-                              child: Image.network(
-                                "${document['images'][0]}",
-                                fit: BoxFit.cover,
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.push(context, MaterialPageRoute(
+                            builder: (context) => ProductPage(productId: document.id,)
+                        ));
+                      },
+                      child: Container(
+                        height: 350.0,
+                        margin: EdgeInsets.symmetric(
+                          vertical: 12.0,
+                          horizontal: 24.0,
+                        ),
+                        child: Stack(
+                          children: [
+                            Container(
+                              height: 350.0,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(12.0),
+                                child: Image.network(
+                                  "${document['images'][0]}",
+                                  fit: BoxFit.cover,
+                                ),
                               ),
                             ),
-                          ),
-                          Positioned(
-                            bottom: 0,
-                            left: 0,
-                            right: 0,
-                            child: Padding(
-                              padding: const EdgeInsets.all(24.0),
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(
-                                    document['name'] ?? "Product Name",
-                                    style: Constants.regularHeading,
-                                  ),
-                                  Text(
-                                    "Rs. ${document['price']}",
-                                    style: TextStyle(
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.w600,
-                                      color: Theme.of(context).accentColor,
+                            Positioned(
+                              bottom: 0,
+                              left: 0,
+                              right: 0,
+                              child: Padding(
+                                padding: const EdgeInsets.all(24.0),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      document['name'] ?? "Product Name",
+                                      style: Constants.regularHeading,
                                     ),
-                                  ),
-                                ],
+                                    Text(
+                                      "Rs. ${document['price']}",
+                                      style: TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.w600,
+                                        color: Theme.of(context).accentColor,
+                                      ),
+                                    ),
+                                  ],
+                                ),
                               ),
-                            ),
-                          )
-                        ],
+                            )
+                          ],
+                        ),
                       ),
                     );
                   }).toList(),
                 );
               }
+
               //Loading state
               return Scaffold(
                 body: Center(
